a57a456c7cd477c5f0ff57ded4bf78e98ddec7b2
Enrique Saracho Felix <esarachof97@gmail.com>
2023-02-20 15:09:08 -0800

    Made blueprint for ToDoList.js return

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,6 +2,19 @@ export default function ToDoList() {
   return (
     <div className="container">
       <h2>To-Do</h2>
+      <div className="newTask">
+        <textarea placeholder="Task..."></textarea>
+        <button>Add</button>
+        <button>Cancel</button>
+      </div>
+      <button>+</button>
+      <ul>
+        <li>
+          Task 1<button>Add</button>
+          <button>Done</button>
+          <button>Delete</button>
+        </li>
+      </ul>
     </div>
   );
 }
