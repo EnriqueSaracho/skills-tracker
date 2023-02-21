29e9a78665f9d2d191f53b63221d0f5a8adc17d1
Enrique Saracho Felix <esarachof97@gmail.com>
2023-02-20 19:27:01 -0800

    Updated ToDoList.js Created ToDoList.css
    Made a form that appears for adding a new task, still working on making the animation smooth

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import "./ToDoList.css";
+
 export default function ToDoList() {
+  const [showNewTask, setShowNewTask] = useState(false);
+
+  const addClicked = () => {
+    setShowNewTask(true);
+  };
+
+  const cancelClicked = () => {
+    setShowNewTask(false);
+  };
+
   return (
     <div className="container">
       <h2>To-Do</h2>
-      <div className="newTask">
-        <textarea placeholder="Task..."></textarea>
-        <button>Add</button>
-        <button>Cancel</button>
-      </div>
-      <button>+</button>
+      {showNewTask && (
+        <div className={showNewTask ? "visible" : "hidden"}>
+          <textarea placeholder="Task..."></textarea>
+          <button>Add</button>
+          <button onClick={cancelClicked}>Cancel</button>
+        </div>
+      )}
+      <button onClick={addClicked}>+</button>
       <ul>
         <li>
           Task 1<button>Add</button>
